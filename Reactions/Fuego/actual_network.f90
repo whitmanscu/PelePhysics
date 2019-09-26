9ef9b4d1b4aa31134b9733c7f077c651a795368e
Bruce Perry <baperry@princeton.edu>
2019-09-26 15:26:54 -0600

    diffusion of Aux variables

--- Reactions/Fuego/actual_network.f90
+++ Reactions/Fuego/actual_network.f90
@@ -1,7 +1,7 @@
 module actual_network
 
   use fuego_chemistry 
-  use chemistry_module, only : nspecies, nelements, nreactions, chemistry_init, &
+  use chemistry_module, only : nspecies, nreactions, chemistry_init, &
        chemistry_close, chemistry_initialized, spec_names, elem_names, &
        L_spec_name, L_elem_name
 
@@ -14,9 +14,29 @@ contains
   
   subroutine actual_network_init
 
+    use extern_probin_module, only: numaux, auxnamesin
+    use amrex_error_module
+
+    implicit none
+
+    integer :: iaux,nnames
+
     if (.not. chemistry_initialized)  call chemistry_init()
-    naux = 0
+
+    ! Get auxiliary variables (if any)
+    naux = numaux
     allocate(aux_names(naux))
+    nnames = count(transfer(auxnamesin, 'a', len(auxnamesin)) == ",") +1
+    if (naux .gt. 0) then 
+       if (naux .ne. nnames) then
+          call amrex_error('simple ::actual_network_init wrong number of aux variable names')
+       end if
+       read(auxnamesin,*) aux_names
+       do iaux = 1,naux
+          aux_names(iaux) = trim(adjustl(aux_names(iaux)))
+          print *, 'aux', iaux, aux_names(iaux)
+       end do
+    end if
 
   end subroutine actual_network_init
 
