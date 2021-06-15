ae876d7712f4e206a8d65ed842865b222cf12ccf
whitmanscu <samuel.whitman@colorado.edu>
2021-06-15 13:14:03 -0500

    add untracked files for new propane mechanism

---
+++ Support/Fuego/Mechanism/Models/Ghani_C3H8/mechanism.cpp
@@ -0,0 +1,510 @@
+#ifndef MECHANISM_CPP
+#define MECHANISM_CPP
+
+#include "mechanism.H"
+
+
+
+/*save atomic weights into array */
+void atomicWeight(amrex::Real *  awt)
+{
+    awt[0] = 12.011150; /*C */
+    awt[1] = 15.999400; /*O */
+    awt[2] = 1.007970; /*H */
+    awt[3] = 14.006700; /*N */
+
+    return;
+}
+
+
+
+/*get atomic weight for all elements */
+void CKAWT( amrex::Real *  awt)
+{
+    atomicWeight(awt);
+}
+
+
+
+/*Returns the elemental composition  */
+/*of the speciesi (mdim is num of elements) */
+void CKNCF(int * ncf)
+{
+    int id; /*loop counter */
+    int kd = 4; 
+    /*Zero ncf */
+    for (id = 0; id < kd * 6; ++ id) {
+         ncf[id] = 0; 
+    }
+
+    /*C3H8 */
+    ncf[ 0 * kd + 0 ] = 3; /*C */
+    ncf[ 0 * kd + 2 ] = 8; /*H */
+
+    /*CO */
+    ncf[ 1 * kd + 0 ] = 1; /*C */
+    ncf[ 1 * kd + 1 ] = 1; /*O */
+
+    /*CO2 */
+    ncf[ 2 * kd + 0 ] = 1; /*C */
+    ncf[ 2 * kd + 1 ] = 2; /*O */
+
+    /*H2O */
+    ncf[ 3 * kd + 2 ] = 2; /*H */
+    ncf[ 3 * kd + 1 ] = 1; /*O */
+
+    /*N2 */
+    ncf[ 4 * kd + 3 ] = 2; /*N */
+
+    /*O2 */
+    ncf[ 5 * kd + 1 ] = 2; /*O */
+
+}
+
+
+/* Returns the vector of strings of element names */
+void CKSYME_STR(amrex::Vector<std::string>& ename)
+{
+    ename.resize(4);
+    ename[0] = "C";
+    ename[1] = "O";
+    ename[2] = "H";
+    ename[3] = "N";
+}
+
+
+/* Returns the vector of strings of species names */
+void CKSYMS_STR(amrex::Vector<std::string>& kname)
+{
+    kname.resize(6);
+    kname[0] = "C3H8";
+    kname[1] = "CO";
+    kname[2] = "CO2";
+    kname[3] = "H2O";
+    kname[4] = "N2";
+    kname[5] = "O2";
+}
+
+/*compute the sparsity pattern of the chemistry Jacobian */
+void SPARSITY_INFO( int * nJdata, int * consP, int NCELLS)
+{
+    amrex::Gpu::DeviceVector<amrex::Real> J_v(49);
+    amrex::Gpu::DeviceVector<amrex::Real> c_v(6);
+    amrex::Real * J_d = J_v.data();
+    amrex::Real * c_d = c_v.data();
+
+    amrex::Real J_h[49];
+
+    amrex::IntVect iv(AMREX_D_DECL(0,0,0));
+    amrex::ParallelFor(amrex::Box(iv,iv),
+        [=] AMREX_GPU_HOST_DEVICE (int /*i*/, int /*j*/, int /*k*/) noexcept {
+            for (int l=0; l<6; l++) {
+                c_d[l] = 1.0/ 6.000000 ;
+            }
+            aJacobian(J_d, c_d, 1500.0, *consP);
+    });
+
+#ifdef AMREX_USE_GPU
+    amrex::Gpu::dtoh_memcpy(J_h, J_d, sizeof(J_d));
+#else
+    std::memcpy(&J_h, J_d, sizeof(J_h));
+#endif
+
+    int nJdata_tmp = 0;
+    for (int k=0; k<7; k++) {
+        for (int l=0; l<7; l++) {
+            if(J_h[ 7 * k + l] != 0.0){
+                nJdata_tmp = nJdata_tmp + 1;
+            }
+        }
+    }
+
+    *nJdata = NCELLS * nJdata_tmp;
+
+    return;
+}
+
+
+
+/*compute the sparsity pattern of the system Jacobian */
+void SPARSITY_INFO_SYST( int * nJdata, int * consP, int NCELLS)
+{
+    amrex::Gpu::DeviceVector<amrex::Real> J_v(49);
+    amrex::Gpu::DeviceVector<amrex::Real> c_v(6);
+    amrex::Real * J_d = J_v.data();
+    amrex::Real * c_d = c_v.data();
+
+    amrex::Real J_h[49];
+
+    amrex::IntVect iv(AMREX_D_DECL(0,0,0));
+    amrex::ParallelFor(amrex::Box(iv,iv),
+        [=] AMREX_GPU_HOST_DEVICE (int /*i*/, int /*j*/, int /*k*/) noexcept {
+            for (int k=0; k<6; k++) {
+                c_d[k] = 1.0/ 6.000000 ;
+            }
+            aJacobian(J_d, c_d, 1500.0, *consP);
+    });
+
+#ifdef AMREX_USE_GPU
+    amrex::Gpu::dtoh_memcpy(J_h, J_d, sizeof(J_d));
+#else
+    std::memcpy(&J_h, J_d, sizeof(J_h));
+#endif
+
+    int nJdata_tmp = 0;
+    for (int k=0; k<7; k++) {
+        for (int l=0; l<7; l++) {
+            if(k == l){
+                nJdata_tmp = nJdata_tmp + 1;
+            } else {
+                if(J_h[ 7 * k + l] != 0.0){
+                    nJdata_tmp = nJdata_tmp + 1;
+                }
+            }
+        }
+    }
+
+    *nJdata = NCELLS * nJdata_tmp;
+
+    return;
+}
+
+
+
+/*compute the sparsity pattern of the simplified (for preconditioning) system Jacobian */
+void SPARSITY_INFO_SYST_SIMPLIFIED( int * nJdata, int * consP)
+{
+    amrex::Gpu::DeviceVector<amrex::Real> J_v(49);
+    amrex::Gpu::DeviceVector<amrex::Real> c_v(6);
+    amrex::Real * J_d = J_v.data();
+    amrex::Real * c_d = c_v.data();
+
+    amrex::Real J_h[49];
+
+    amrex::IntVect iv(AMREX_D_DECL(0,0,0));
+    amrex::ParallelFor(amrex::Box(iv,iv),
+        [=] AMREX_GPU_HOST_DEVICE (int /*i*/, int /*j*/, int /*k*/) noexcept {
+            for (int k=0; k<6; k++) {
+                c_d[k] = 1.0/ 6.000000 ;
+            }
+            aJacobian_precond(J_d, c_d, 1500.0, *consP);
+    });
+
+#ifdef AMREX_USE_GPU
+    amrex::Gpu::dtoh_memcpy(J_h, J_d, sizeof(J_d));
+#else
+    std::memcpy(&J_h, J_d, sizeof(J_h));
+#endif
+
+    int nJdata_tmp = 0;
+    for (int k=0; k<7; k++) {
+        for (int l=0; l<7; l++) {
+            if(k == l){
+                nJdata_tmp = nJdata_tmp + 1;
+            } else {
+                if(J_h[ 7 * k + l] != 0.0){
+                    nJdata_tmp = nJdata_tmp + 1;
+                }
+            }
+        }
+    }
+
+    nJdata[0] = nJdata_tmp;
+
+    return;
+}
+
+
+/*compute the sparsity pattern of the chemistry Jacobian in CSC format -- base 0 */
+void SPARSITY_PREPROC_CSC(int *  rowVals, int *  colPtrs, int * consP, int NCELLS)
+{
+    int offset_row;
+    int offset_col;
+
+    amrex::Gpu::DeviceVector<amrex::Real> J_v(49);
+    amrex::Gpu::DeviceVector<amrex::Real> c_v(6);
+    amrex::Real * J_d = J_v.data();
+    amrex::Real * c_d = c_v.data();
+
+    amrex::Real J_h[49];
+
+    amrex::IntVect iv(AMREX_D_DECL(0,0,0));
+    amrex::ParallelFor(amrex::Box(iv,iv),
+        [=] AMREX_GPU_HOST_DEVICE (int /*i*/, int /*j*/, int /*k*/) noexcept {
+            for (int k=0; k<6; k++) {
+                c_d[k] = 1.0/ 6.000000 ;
+            }
+            aJacobian(J_d, c_d, 1500.0, *consP);
+    });
+
+#ifdef AMREX_USE_GPU
+    amrex::Gpu::dtoh_memcpy(J_h, J_d, sizeof(J_d));
+#else
+    std::memcpy(&J_h, J_d, sizeof(J_h));
+#endif
+
+    colPtrs[0] = 0;
+    int nJdata_tmp = 0;
+    for (int nc=0; nc<NCELLS; nc++) {
+        offset_row = nc * 7;
+        offset_col = nc * 7;
+        for (int k=0; k<7; k++) {
+            for (int l=0; l<7; l++) {
+                if(J_h[7*k + l] != 0.0) {
+                    rowVals[nJdata_tmp] = l + offset_row; 
+                    nJdata_tmp = nJdata_tmp + 1; 
+                }
+            }
+            colPtrs[offset_col + (k + 1)] = nJdata_tmp;
+        }
+    }
+
+    return;
+}
+
+/*compute the sparsity pattern of the chemistry Jacobian in CSR format -- base 0 */
+void SPARSITY_PREPROC_CSR(int * colVals, int * rowPtrs, int * consP, int NCELLS, int base)
+{
+    int offset;
+    amrex::Gpu::DeviceVector<amrex::Real> J_v(49);
+    amrex::Gpu::DeviceVector<amrex::Real> c_v(6);
+    amrex::Real * J_d = J_v.data();
+    amrex::Real * c_d = c_v.data();
+
+    amrex::Real J_h[49];
+
+    amrex::IntVect iv(AMREX_D_DECL(0,0,0));
+    amrex::ParallelFor(amrex::Box(iv,iv),
+        [=] AMREX_GPU_HOST_DEVICE (int /*i*/, int /*j*/, int /*k*/) noexcept {
+            for (int k=0; k<6; k++) {
+                c_d[k] = 1.0/ 6.000000 ;
+            }
+            aJacobian(J_d, c_d, 1500.0, *consP);
+    });
+
+#ifdef AMREX_USE_GPU
+    amrex::Gpu::dtoh_memcpy(J_h, J_d, sizeof(J_d));
+#else
+    std::memcpy(&J_h, J_d, sizeof(J_h));
+#endif
+
+    if (base == 1) {
+        rowPtrs[0] = 1;
+        int nJdata_tmp = 1;
+        for (int nc=0; nc<NCELLS; nc++) {
+            offset = nc * 7;
+            for (int l=0; l<7; l++) {
+                for (int k=0; k<7; k++) {
+                    if(J_h[7*k + l] != 0.0) {
+                        colVals[nJdata_tmp-1] = k+1 + offset; 
+                        nJdata_tmp = nJdata_tmp + 1; 
+                    }
+                }
+                rowPtrs[offset + (l + 1)] = nJdata_tmp;
+            }
+        }
+    } else {
+        rowPtrs[0] = 0;
+        int nJdata_tmp = 0;
+        for (int nc=0; nc<NCELLS; nc++) {
+            offset = nc * 7;
+            for (int l=0; l<7; l++) {
+                for (int k=0; k<7; k++) {
+                    if(J_h[7*k + l] != 0.0) {
+                        colVals[nJdata_tmp] = k + offset; 
+                        nJdata_tmp = nJdata_tmp + 1; 
+                    }
+                }
+                rowPtrs[offset + (l + 1)] = nJdata_tmp;
+            }
+        }
+    }
+
+    return;
+}
+
+/*compute the sparsity pattern of the system Jacobian */
+/*CSR format BASE is user choice */
+void SPARSITY_PREPROC_SYST_CSR(int * colVals, int * rowPtr, int * consP, int NCELLS, int base)
+{
+    int offset;
+    amrex::Gpu::DeviceVector<amrex::Real> J_v(49);
+    amrex::Gpu::DeviceVector<amrex::Real> c_v(6);
+    amrex::Real * J_d = J_v.data();
+    amrex::Real * c_d = c_v.data();
+
+    amrex::Real J_h[49];
+
+    amrex::IntVect iv(AMREX_D_DECL(0,0,0));
+    amrex::ParallelFor(amrex::Box(iv,iv),
+        [=] AMREX_GPU_HOST_DEVICE (int /*i*/, int /*j*/, int /*k*/) noexcept {
+            for (int k=0; k<6; k++) {
+                c_d[k] = 1.0/ 6.000000 ;
+            }
+            aJacobian(J_d, c_d, 1500.0, *consP);
+    });
+
+#ifdef AMREX_USE_GPU
+    amrex::Gpu::dtoh_memcpy(J_h, J_d, sizeof(J_d));
+#else
+    std::memcpy(&J_h, J_d, sizeof(J_h));
+#endif
+    if (base == 1) {
+        rowPtr[0] = 1;
+        int nJdata_tmp = 1;
+        for (int nc=0; nc<NCELLS; nc++) {
+            offset = nc * 7;
+            for (int l=0; l<7; l++) {
+                for (int k=0; k<7; k++) {
+                    if (k == l) {
+                        colVals[nJdata_tmp-1] = l+1 + offset; 
+                        nJdata_tmp = nJdata_tmp + 1; 
+                    } else {
+                        if(J_h[7*k + l] != 0.0) {
+                            colVals[nJdata_tmp-1] = k+1 + offset; 
+                            nJdata_tmp = nJdata_tmp + 1; 
+                        }
+                    }
+                }
+                rowPtr[offset + (l + 1)] = nJdata_tmp;
+            }
+        }
+    } else {
+        rowPtr[0] = 0;
+        int nJdata_tmp = 0;
+        for (int nc=0; nc<NCELLS; nc++) {
+            offset = nc * 7;
+            for (int l=0; l<7; l++) {
+                for (int k=0; k<7; k++) {
+                    if (k == l) {
+                        colVals[nJdata_tmp] = l + offset; 
+                        nJdata_tmp = nJdata_tmp + 1; 
+                    } else {
+                        if(J_h[7*k + l] != 0.0) {
+                            colVals[nJdata_tmp] = k + offset; 
+                            nJdata_tmp = nJdata_tmp + 1; 
+                        }
+                    }
+                }
+                rowPtr[offset + (l + 1)] = nJdata_tmp;
+            }
+        }
+    }
+
+    return;
+}
+
+/*compute the sparsity pattern of the simplified (for precond) system Jacobian on CPU */
+/*BASE 0 */
+void SPARSITY_PREPROC_SYST_SIMPLIFIED_CSC(int * rowVals, int * colPtrs, int * indx, int * consP)
+{
+    amrex::Gpu::DeviceVector<amrex::Real> J_v(49);
+    amrex::Gpu::DeviceVector<amrex::Real> c_v(6);
+    amrex::Real * J_d = J_v.data();
+    amrex::Real * c_d = c_v.data();
+
+    amrex::Real J_h[49];
+
+    amrex::IntVect iv(AMREX_D_DECL(0,0,0));
+    amrex::ParallelFor(amrex::Box(iv,iv),
+        [=] AMREX_GPU_HOST_DEVICE (int /*i*/, int /*j*/, int /*k*/) noexcept {
+            for (int k=0; k<6; k++) {
+                c_d[k] = 1.0/ 6.000000 ;
+            }
+            aJacobian_precond(J_d, c_d, 1500.0, *consP);
+    });
+
+#ifdef AMREX_USE_GPU
+    amrex::Gpu::dtoh_memcpy(J_h, J_d, sizeof(J_d));
+#else
+    std::memcpy(&J_h, J_d, sizeof(J_h));
+#endif
+
+    colPtrs[0] = 0;
+    int nJdata_tmp = 0;
+    for (int k=0; k<7; k++) {
+        for (int l=0; l<7; l++) {
+            if (k == l) {
+                rowVals[nJdata_tmp] = l; 
+                indx[nJdata_tmp] = 7*k + l;
+                nJdata_tmp = nJdata_tmp + 1; 
+            } else {
+                if(J_h[7*k + l] != 0.0) {
+                    rowVals[nJdata_tmp] = l; 
+                    indx[nJdata_tmp] = 7*k + l;
+                    nJdata_tmp = nJdata_tmp + 1; 
+                }
+            }
+        }
+        colPtrs[k+1] = nJdata_tmp;
+    }
+
+    return;
+}
+
+/*compute the sparsity pattern of the simplified (for precond) system Jacobian */
+/*CSR format BASE is under choice */
+void SPARSITY_PREPROC_SYST_SIMPLIFIED_CSR(int * colVals, int * rowPtr, int * consP, int base)
+{
+    amrex::Gpu::DeviceVector<amrex::Real> J_v(49);
+    amrex::Gpu::DeviceVector<amrex::Real> c_v(6);
+    amrex::Real * J_d = J_v.data();
+    amrex::Real * c_d = c_v.data();
+
+    amrex::Real J_h[49];
+
+    amrex::IntVect iv(AMREX_D_DECL(0,0,0));
+    amrex::ParallelFor(amrex::Box(iv,iv),
+        [=] AMREX_GPU_HOST_DEVICE (int /*i*/, int /*j*/, int /*k*/) noexcept {
+            for (int k=0; k<6; k++) {
+                c_d[k] = 1.0/ 6.000000 ;
+            }
+            aJacobian_precond(J_d, c_d, 1500.0, *consP);
+    });
+
+#ifdef AMREX_USE_GPU
+    amrex::Gpu::dtoh_memcpy(J_h, J_d, sizeof(J_d));
+#else
+    std::memcpy(&J_h, J_d, sizeof(J_h));
+#endif
+
+    if (base == 1) {
+        rowPtr[0] = 1;
+        int nJdata_tmp = 1;
+        for (int l=0; l<7; l++) {
+            for (int k=0; k<7; k++) {
+                if (k == l) {
+                    colVals[nJdata_tmp-1] = l+1; 
+                    nJdata_tmp = nJdata_tmp + 1; 
+                } else {
+                    if(J_h[7*k + l] != 0.0) {
+                        colVals[nJdata_tmp-1] = k+1; 
+                        nJdata_tmp = nJdata_tmp + 1; 
+                    }
+                }
+            }
+            rowPtr[l+1] = nJdata_tmp;
+        }
+    } else {
+        rowPtr[0] = 0;
+        int nJdata_tmp = 0;
+        for (int l=0; l<7; l++) {
+            for (int k=0; k<7; k++) {
+                if (k == l) {
+                    colVals[nJdata_tmp] = l; 
+                    nJdata_tmp = nJdata_tmp + 1; 
+                } else {
+                    if(J_h[7*k + l] != 0.0) {
+                        colVals[nJdata_tmp] = k; 
+                        nJdata_tmp = nJdata_tmp + 1; 
+                    }
+                }
+            }
+            rowPtr[l+1] = nJdata_tmp;
+        }
+    }
+
+    return;
+}
+
+#endif
